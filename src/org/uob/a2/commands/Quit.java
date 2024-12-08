521cb88b6eea494222e5d0fbae27ec56460840a2
Motty S <mottyschneck8@gmail.com>
2024-12-08 15:58:36 +0000

    removed throws IOException

--- src/org/uob/a2/commands/Quit.java
+++ src/org/uob/a2/commands/Quit.java
@@ -20,40 +20,42 @@ public class Quit extends Command {
         }
 
         @Override
-        public String execute(GameState gameState) throws IOException {
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter("gamestate.txt"))) {
-                    writer.write("player:" + gameState.getPlayer().getName() + "\n");
-                    writer.write("map:" + gameState.getMap().getRooms().stream().map(Room::getName).collect(Collectors.joining(",")) + "\n");
-
-                    for (Room room : gameState.getMap().getRooms()) {
-                        writer.write("room:" + room.getId() + "," + room.getName() + "," + room.getDescription() + "\n");
-                        for (Item item : room.getItems()) {
-                            writer.write("item:" + item.getId() + "," + item.getName() + "," + item.getDescription() + "," + item.getHidden() + "\n");
-                        }
-                        for (Feature feature : room.getFeatures()) {
-                            if (feature instanceof Container) {
-                                Container container = (Container) feature;
-                                writer.write("container:" + container.getId() + "," + container.getName() + "," + container.getDescription() + "\n");
-                            }
-                        }
-                        for (Exit exit : room.getExits()) {
-                            writer.write("exit:" + exit.getId() + "," + exit.getName() + "," + exit.getDescription() + "," + exit.getNextRoom() + "\n");
-                        }
-                    }
-
-                    for (Equipment equipment : gameState.getPlayer().getEquipment()) {
-                        UseInformation useInfo = equipment.getUseInformation();
-                        writer.write("equipment:" + equipment.getId()  + "," + equipment.getName() + "," + equipment.getDescription() + "," +
-                                useInfo.isUsed() + "," + useInfo.getAction() + "," + useInfo.getTarget() + "," + useInfo.getResult() + "," + useInfo.getMessage() + "\n");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        public String execute(GameState gameState){
+            saveGame(gameState);
             return "You have quit the game. Thank you for playing!\n";
 
 
         }
+        public void saveGame(GameState gameState) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("gamestate.txt"))) {
+                writer.write("player:" + gameState.getPlayer().getName() + "\n");
+                writer.write("map:" + gameState.getMap().getRooms().stream().map(Room::getName).collect(Collectors.joining(",")) + "\n");
 
+                for (Room room : gameState.getMap().getRooms()) {
+                    writer.write("room:" + room.getId() + "," + room.getName() + "," + room.getDescription() + "\n");
+                    for (Item item : room.getItems()) {
+                        writer.write("item:" + item.getId() + "," + item.getName() + "," + item.getDescription() + "," + item.getHidden() + "\n");
+                    }
+                    for (Feature feature : room.getFeatures()) {
+                        if (feature instanceof Container) {
+                            Container container = (Container) feature;
+                            writer.write("container:" + container.getId() + "," + container.getName() + "," + container.getDescription() + "\n");
+                        }
+                    }
+                    for (Exit exit : room.getExits()) {
+                        writer.write("exit:" + exit.getId() + "," + exit.getName() + "," + exit.getDescription() + "," + exit.getNextRoom() + "\n");
+                    }
+                }
+
+                for (Equipment equipment : gameState.getPlayer().getEquipment()) {
+                    UseInformation useInfo = equipment.getUseInformation();
+                    writer.write("equipment:" + equipment.getId()  + "," + equipment.getName() + "," + equipment.getDescription() + "," +
+                            useInfo.isUsed() + "," + useInfo.getAction() + "," + useInfo.getTarget() + "," + useInfo.getResult() + "," + useInfo.getMessage() + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         @Override
         public String toString() {
             return "The quit command.";
