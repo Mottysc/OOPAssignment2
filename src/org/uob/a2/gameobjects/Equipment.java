d0692ac6404e1f6fe8bd49e44b4e5a00caad4d7b
Motty S <mottyschneck8@gmail.com>
2024-12-10 13:57:42 +0000

    working through tests

--- src/org/uob/a2/gameobjects/Equipment.java
+++ src/org/uob/a2/gameobjects/Equipment.java
@@ -23,7 +23,7 @@ public class Equipment extends GameObject implements Usable {
             this.useInformation.setUsed(true);
             if (useInformation.getAction().equals("open")){
                 gameState.getMap().getCurrentRoom().getAll().stream()
-                        .filter(obj -> obj.getId().equals(target.getId()))
+                        .filter(obj -> obj.getId().equals(useInformation.getTarget()))
                         .forEach(obj -> obj.setHidden(false));
                 return useInformation.getMessage();
 
