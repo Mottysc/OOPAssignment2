01a27b2e3177f628651135d186248cad60c68482
Motty S <mottyschneck8@gmail.com>
2024-12-11 15:03:23 +0000

    quit

--- src/org/uob/a2/parser/Parser.java
+++ src/org/uob/a2/parser/Parser.java
@@ -74,7 +74,7 @@ public class Parser {
                 return new Help(tokens.get(1).getTokenType().name());
             }
             case QUIT -> {
-                if (tokens.size() != 2 || !tokens.get(1).getValue().equalsIgnoreCase("save")){
+                if (tokens.size() != 2 && !tokens.get(1).getValue().equalsIgnoreCase("save")){
                     throw new CommandErrorException("Invalid QUIT command format. Expected: QUIT [SAVE]");
                 }
                 return new Quit(tokens.get(1).getValue());
